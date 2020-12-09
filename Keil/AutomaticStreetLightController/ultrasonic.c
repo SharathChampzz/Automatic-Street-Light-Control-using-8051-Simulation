e9b31ba787b1a4edbe0cb242f34ad919a930bb28
SharathChampzz <sharathkumar.mskj.1999@gmail.com>
2020-12-09 18:47:43 +0530

    Added Ultrasonic Sensor

--- Keil/AutomaticStreetLightController/ultrasonic.c
+++ Keil/AutomaticStreetLightController/ultrasonic.c
@@ -1,29 +1,13 @@
 #include<reg51.h>
-sbit sensor1 = P1^0;
-sbit sensor2 = P1^1;
-sbit sensor3 = P1^2;
-
-sbit load1 = P2^0;
-sbit load2 = P2^1;
-sbit load3 = P2^2;
-
-sbit ldr = P1^7;
-sbit ldrout = P2^3;
-
-
 sbit trigpin = P2^6;
 sbit echopin = P2^7;
 sbit led1 = P1^5;
-sbit led2 = P1^6;
+sbit led2 = P1^7;
+
 void delay(unsigned int z);
 unsigned int distance,i;
-
 void main(){
-	load1 = load2 = load3 = 0;
-	sensor1 = sensor2 = sensor3 = 0;
-	ldr = ldrout = 0;
-	
-			echopin=1;
+      echopin=1;
       led1=0;
       led2=0;
       trigpin=0;
@@ -33,45 +17,11 @@ void main(){
       TMOD=0x01;
       TH0=0x00;
       TL0=0x00;
-	
-	while(1){
-		
-		if(ldr == 0){ // No Light means Night time
-			ldrout = 1; // during night time green led turns on just for indication
-			
-		if(sensor1 == 0 && sensor2 == 0 && sensor3 == 0){
-			load1 = load2 = load3 = 0;
-		}
-		if(sensor1 == 1){
-			load1 = 1;
-		}
-		
-		if(sensor2 == 1){
-			load2 = 1;
-		}
-		
-		if(sensor3 == 1){
-			load3 = 1;
-		}
-		if(sensor1 == 0){
-			load1 = 0;
-		}
-		
-		if(sensor2 == 0){
-			load2 = 0;
-		}
-		
-		if(sensor3 == 0){
-			load3 = 0;
-		}
-		}
-		else{  // there is a light 
-			ldrout = 0;
-			load1 = load2 = load3 = 0;
-		}  
 
 
-						trigpin=1;
+      while(1)
+      {
+            trigpin=1;
             for(i=0;i<=10;i++);
             trigpin=0;
             while(echopin==0);
@@ -106,10 +56,9 @@ void main(){
             {
                 led2=0;
             }
+      }
+}                                                             
 
-							
-}
-}
 
 void delay(unsigned int z)
 {
@@ -118,4 +67,4 @@ void delay(unsigned int z)
     {
       for(q=0;q<1375;q++); //repeat for 1375 times
     }
-}
+}                   
